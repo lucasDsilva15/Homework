55fc3bfc12b5fe00d09240f6e174562771a0800c
Lucas Da Silva <lucasilva15@live.com>
2022-09-04 14:34:37 -0400

    finished lightswitch lab

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -4,22 +4,34 @@ let title = document.querySelector(".container");
 let image = title.querySelectorAll("img");
 let container = document.querySelector('.lightswitch')
 let lightSwitch = body.querySelector('img')
+let buttoncont = document.querySelector('.buttoncont')
+let rgb = buttoncont.querySelector('button')
+let text = 'lights on';
 
-let text = 'lights off';
-    container.addEventListener ('click', changeLights)
-        function changeLights () {
-            body.classList.toggle('black')
-            title.classList.toggle('black')  
-            if (text === 'lights off'){
-                text = 'Lights on'
-                lightSwitch.src = 'Images/light-switch-off.png'
-                image.forEach((element) => element.src = "Images/light-bulb-on.png"
-                )
-                
-            } else {
+
+container.addEventListener ('click', changeLights)
+    function changeLights () {
+        body.classList.toggle('black')
+        title.classList.toggle('black')
+        buttoncont.classList.toggle('black')  
+            if (text === 'lights on'){
                 text = 'lights off'
+                lightSwitch.src = 'Images/light-switch-off.png'
+                image.forEach((element) => element.src = "Images/light-bulb-off.png")
+                
+            } else if (text === 'lights off'){
+                text = 'lights on'
                 lightSwitch.src = 'Images/light-switch-on.png'
-                image.forEach((element) => element.src = "Images/light-bulb-off.png"
-                )
-            }   
+                image.forEach((element) => element.src = "Images/light-bulb-on.png")
+    }   
+}
+rgb.addEventListener('click', rgbLights)
+    function rgbLights () {
+        if (text === 'lights on' || 'lights off'){
+            body.classList.toggle('color0')
+        title.classList.toggle('color0')
+        buttoncont.classList.toggle('color0') 
         }
+    }
+
+
