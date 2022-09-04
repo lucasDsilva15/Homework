818f0a39991362f8a1b623dd863c6aa716225f83
Lucas Da Silva <lucasilva15@live.com>
2022-09-04 14:41:15 -0400

    finished lightswitch lab

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -8,30 +8,29 @@ let buttoncont = document.querySelector('.buttoncont')
 let rgb = buttoncont.querySelector('button')
 let text = 'lights on';
 
-
 container.addEventListener ('click', changeLights)
-    function changeLights () {
-        body.classList.toggle('black')
-        title.classList.toggle('black')
-        buttoncont.classList.toggle('black')  
-            if (text === 'lights on'){
-                text = 'lights off'
-                lightSwitch.src = 'Images/light-switch-off.png'
-                image.forEach((element) => element.src = "Images/light-bulb-off.png")
+function changeLights () {
+    body.classList.toggle('black')
+    title.classList.toggle('black')
+    buttoncont.classList.toggle('black')  
+        if (text === 'lights on'){
+            text = 'lights off'
+            lightSwitch.src = 'Images/light-switch-off.png'
+            image.forEach((element) => element.src = "Images/light-bulb-off.png")
                 
-            } else if (text === 'lights off'){
-                text = 'lights on'
-                lightSwitch.src = 'Images/light-switch-on.png'
-                image.forEach((element) => element.src = "Images/light-bulb-on.png")
-    }   
+        } else if (text === 'lights off'){
+            text = 'lights on'
+            lightSwitch.src = 'Images/light-switch-on.png'
+            image.forEach((element) => element.src = "Images/light-bulb-on.png")
+        }   
 }
 rgb.addEventListener('click', rgbLights)
-    function rgbLights () {
-        if (text === 'lights on' || 'lights off'){
-            body.classList.toggle('color0')
+function rgbLights () {
+    if (text === 'lights on' || 'lights off'){
+        body.classList.toggle('color0')
         title.classList.toggle('color0')
         buttoncont.classList.toggle('color0') 
-        }
     }
+}
 
 
